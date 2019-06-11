36d21caf5a8d6f268f976679210827e8f350232f
baaabuuu <aradar@live.dk>
2019-06-11 11:37:31 +0200

    Updated some dependencies

--- src/main/java/dtu/ttnCommunication/MSGrecver.java
+++ src/main/java/dtu/ttnCommunication/MSGrecver.java
@@ -11,7 +11,6 @@ import org.thethingsnetwork.data.mqtt.Client;
 
 public class MSGrecver
 {
-	private Client client;
 	String region = "eu";
 	String appID = "dtu_nwa";
 	String appAccessKey = "ttn-account-v2.3Npyl34nrqhAQ8YT_A0eJapgYVMCzeOoR82H1XsnZDU";
